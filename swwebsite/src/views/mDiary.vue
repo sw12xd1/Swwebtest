856fa6188a1e1a91d4fae58f70302540001e6e73
sw12xd1 <sw12xd1@gmail.com>
2026-06-22 17:22:48 +0800

    up11-p

--- swwebsite/src/views/mDiary.vue
+++ swwebsite/src/views/mDiary.vue
@@ -109,7 +109,7 @@ onMounted(() => getPageData())
 :deep(.el-dialog) {
   width: 92% !important;
   max-width: 92vw !important;
-  top: -14vh !important;
+  top: -20vh !important;
 }
 :deep(.el-dialog__body) {
   padding: 16px 12px;
